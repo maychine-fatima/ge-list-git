e369e50af9aa14519e78a68bc9d47c9e59abea4d
maychine <fatimamaychine@gmail.com>
2020-06-04 02:39:15 +0100

    github and repo model

---
+++ src/app/models/githubModel.ts
@@ -0,0 +1,7 @@
+export interface githubModel {
+
+    incomplete_results: Boolean,
+    items: object[],
+    total_count: Number
+
+}
---
+++ src/app/models/repoMdel.ts
@@ -0,0 +1,13 @@
+
+
+export interface repoModel {
+
+    avatarOwner: string;
+    repoName: string;
+    repoDescription: string;
+    nbStars: Number;
+    nbIssues: Number;
+    intervalTime: Number;
+    ownerName: string;
+
+}
